59e93386c4515f15bdf112cc00fd94a0a1b71c39
Hasan-sathuli <113457494+Hasan-sathuli@users.noreply.github.com>
2022-10-03 19:18:34 +0530

    Add files via upload

---
+++ Assessments/Team Leader-Hasan sathuli H/Assignment 2/Code.py
@@ -0,0 +1,13 @@
+import random
+
+while(True):
+    temp=random.randint(1,100)
+    humi=random.randint(1,100)
+    print("Temperature :",temp,"  Humidity :",humi)
+    if(temp>25 and humi<40):
+        print("High Temperature!!!  ALARM ON...")
+        print("")
+    else:
+        print("Low Temperature      ALARM OFF...")
+        print("")
+   
